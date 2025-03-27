fe86f12409e0b1adbb601fd626eb61e1fe72ed34
Roshan <71066704+roshan194@users.noreply.github.com>
2025-03-27 23:27:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+function secondHighest(arr) {
+    if (arr.length < 2) return -Infinity; // Edge case: empty or single-element array
+
+    let max = -Infinity, secondMax = -Infinity;
+
+    for (let num of arr) {
+        if (num > max) {
+            secondMax = max;
+            max = num;
+        } else if (num > secondMax && num < max) {
+            secondMax = num;
+        }
+    }
+
+    return secondMax === -Infinity ? -Infinity : secondMax;
+}
